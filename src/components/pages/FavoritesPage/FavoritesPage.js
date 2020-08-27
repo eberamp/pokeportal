bc1ff80ab7ef918b9cefc13bf35981f15b1c23c1
Eber Alejandro Martinez Pantoja <Eber Alejandro Martinez Pantoja>
2020-08-27 11:51:32 -0500

    forgot to revert back the amount (15) needed to display the filter in the favorites page

--- src/components/pages/FavoritesPage/FavoritesPage.js
+++ src/components/pages/FavoritesPage/FavoritesPage.js
@@ -57,7 +57,7 @@ class FavoritesPage extends React.Component {
   }
 
   _renderFilter = () => {
-    const FAVORITE_MAX_COUNT = 3;
+    const FAVORITE_MAX_COUNT = 15; // TODO: needs a better name
     if(this.state.savedFavorites.length > FAVORITE_MAX_COUNT){
       return <TextInput placeHolder="Nombre de pokemon" onChange={this.updateFilterText} />;
     }
