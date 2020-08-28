ecc0b4cf42e6d35dc3689366968dc1f349830c7a
Eber Alejandro Martinez Pantoja <Eber Alejandro Martinez Pantoja>
2020-08-28 18:01:46 -0500

    added property 'disabled' to allow the button to appear disabled

--- src/components/views/Button/Button.js
+++ src/components/views/Button/Button.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Button = function(props) {
-	return <button className={props.className} onClick={props.onClick}>{ props.text }</button>
+	return <button disabled={props.disabled} className={props.className} onClick={props.onClick}>{ props.text }</button>
 }
 
 export { Button };
